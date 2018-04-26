2017df324cc45caa39dc49fca19be14efc5a736c
Smith Alexis Carvajal Orozco <scarvaj9@eafit.edu.co>
2018-04-26 15:25:00 -0500

    filtrado de moto

---
+++ RapidRepuestos/RapidRepuestos/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace RapidRepuestos
+{
+    using Xamarin.Forms;
+    using Views;
+
+    public partial class App : Application
+	{
+        
+        public App ()
+		{
+			InitializeComponent();
+
+            this.MainPage = new FiltroMotoPage();
+		}
+
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ RapidRepuestos/RapidRepuestos/Views/FiltroMotoPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RapidRepuestos.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FiltroMotoPage : ContentPage
+	{
+		public FiltroMotoPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
