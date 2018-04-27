5bbe273831fbda67ddaf6aee8a9a11a9e16e1bab
Smith Alexis Carvajal Orozco <scarvaj9@eafit.edu.co>
2018-04-27 02:22:26 -0500

    darle fida a filtrar moto

---
+++ RapidRepuestos/RapidRepuestos/ViewModels/FiltrarMotoViewModels.cs
@@ -0,0 +1,79 @@
+﻿
+
+namespace RapidRepuestos.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using System;
+    using System.ComponentModel;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
+    public class FiltrarMotoViewModels
+    {     
+
+
+        //propiedades de filtrar moto que vienen de filtromotopage
+
+        public bool Prueba { get; set; }
+
+        public string MarcaMoto { get; set; }
+
+        public string TipoMoto { get; set; }
+
+        public string MotoExacta { get; set; }
+
+
+        // aca vamos a poner los constructores 
+        public FiltrarMotoViewModels()
+        {
+            this.Prueba = true;
+
+        }
+
+        // los botones o commands los ponemos por aparte pero tambien son propiedades
+        public ICommand BuscarMoto {
+            get
+            {
+                return new RelayCommand(Buscar);
+            }
+        }
+
+        
+        
+        private async void Buscar()
+        {
+            if (string.IsNullOrEmpty(this.MarcaMoto))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "no ingreso la marca de la moto",
+                    "Aceptar");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.TipoMoto))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "no ingreso el tipo de moto",
+                    "Aceptar");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.MotoExacta))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "no ingreso el nombre de la moto ",
+                    "Aceptar");
+                return;
+            }
+
+
+
+        }
+
+       
+        
+    }
+}
--- RapidRepuestos/RapidRepuestos/ViewModels/MainViewModels.cs
+++ RapidRepuestos/RapidRepuestos/ViewModels/MainViewModels.cs
@@ -1,7 +1,19 @@
 ﻿namespace RapidRepuestos.ViewModels
 {
+
     public class MainViewModels
     {
+        public FiltrarMotoViewModels Filtrar { get; set; }
 
-    }
+
+        public MainViewModels()
+        {
+
+            this.Filtrar = new FiltrarMotoViewModels();
+
+        }
+
+
+    }   
+
 }
